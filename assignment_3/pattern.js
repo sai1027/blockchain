d3e97e034d5d25b065de4d2443c3ae0819cd20c0
sai teja <venkatsaiteja1027@gmail.com>
2022-08-27 17:04:05 +0530

    solved

---
+++ assignment_3/pattern.js
@@ -0,0 +1,19 @@
+var n=1;
+
+for (let i = 1; i < 5; i++){
+    for(let j=1; j<=i; j++){
+        process.stdout.write(n + " ");
+        n++;
+        
+    }
+  console.log();
+}; 
+
+
+
+
+// output 
+// 1
+// 2 3
+// 4 5 6
+// 7 8 9 10
