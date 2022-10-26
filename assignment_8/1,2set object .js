379b43ec385dd7abfd4a67007e29e8f0289e0e41
sai teja <venkatsaiteja1027@gmail.com>
2022-10-26 17:40:07 +0530

    week 8 completed

---
+++ assignment_8/1,2set object .js
@@ -0,0 +1,12 @@
+// 1) 
+// No . Duplicate values are not allowed in set
+
+// 2) creating, adding, deleting element
+
+let a = new Set(['a','b']);
+
+a.add("c");
+console.log(a);
+
+a.delete("a");
+console.log(a);
